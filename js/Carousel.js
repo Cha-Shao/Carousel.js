3f7596fc89937956ab94bdb103c7cc58cec8f955
Cha_Shao <yrchashao@outlook.com>
2022-03-08 15:22:01 +0800

    上一页也会更新页码啦

--- js/Carousel.js
+++ js/Carousel.js
@@ -1,15 +1,16 @@
 var carouselTime = 7000 //轮播切换的速度(默认7s)
 
-function sleep(millisecond) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve()
-        }, millisecond)
-    })
-}
+// function sleep(millisecond) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve()
+//         }, millisecond)
+//     })
+// }
 
 const items = document.querySelectorAll("[c-item]")
 const carousel = document.querySelector("[carousel]")
+const carouselPoint = document.getElementsByClassName('carouselPoint')
 let i = 0;
 
 // 自动轮播
@@ -23,17 +24,22 @@ if (document.querySelector("[c-autoplay]")){
 function carouselPrevious(){
     i--
     if (i < 0) i = items.length-1;
+    const _i = i
     carousel.style.transform = "translateX(-"+i+"00%)";
+    if (document.querySelector("[c-point]")){
+        for (let i = 0; i < items.length; i++){
+            carouselPoint[i].style.cssText='width: 9px;height:9px;background:lightgray;border-radius:100%;margin:5px;z-index:2;cursor:pointer;'}
+        carouselPoint[_i].style.background='white'
+    }
 }
 function carouselNext(){
     i++
     if (i > items.length-1) i = 0;
     const _i = i
     carousel.style.transform = "translateX(-"+i+"00%)";
+    // 如果开启了页码就渲染切换
     if (document.querySelector("[c-point]")){
-        const carouselPoint = document.getElementsByClassName('carouselPoint')
         for (let i = 0; i < items.length; i++){
-            const carouselPoint = document.getElementsByClassName('carouselPoint')
             carouselPoint[i].style.cssText='width: 9px;height:9px;background:lightgray;border-radius:100%;margin:5px;z-index:2;cursor:pointer;'}
         carouselPoint[_i].style.background='white'
     }
@@ -42,6 +48,7 @@ function carouselNext(){
 // 设置轮播页下面的点
 function setCarouselPointContainer() {
     const carouselPointContainer = document.querySelector("[c-point]");
+    // 如果存在
     if (carouselPointContainer) {
         const appendCarouselPointContainer = document.createElement("div")
         appendCarouselPointContainer.className = 'carouselPointContainer'
@@ -50,6 +57,7 @@ function setCarouselPointContainer() {
     }
 }
 function setCarouselPoint(){
+    // 添加页码container
     for (let i = 0; i < items.length; i++) {
         const carouselPointContainer = document.getElementsByClassName("carouselPointContainer")
         const appendCarouselPoint = document.createElement("div")
@@ -58,18 +66,22 @@ function setCarouselPoint(){
         carouselPointContainer[0].style.cssText="display:flex;position:absolute;bottom:12px;"
         if (document.querySelector("[point-right]")) carouselPointContainer[0].style.right="27px"; else {carouselPointContainer[0].style.left="50%";carouselPointContainer[0].style.transform="translateX(-50%)"}
     }
+    // 添加页码按钮
     for (let i = 0; i < items.length; i++){
-        const carouselPoint = document.getElementsByClassName('carouselPoint')
         carouselPoint[i].setAttribute("onclick","setCarouselPage("+i+")")
         carouselPoint[i].setAttribute("Page",i)
         carouselPoint[i].style.cssText='width: 9px;height:9px;background:lightgray;border-radius:100%;margin:5px;z-index:2;cursor:pointer;'
     }
 }
 
-// 页码点跳转
+// 点击页码后
 function setCarouselPage(setPage){
     i = setPage
+    const _i = i
     carousel.style.transform = "translateX(-"+i+"00%)";
+    for (let i = 0; i < items.length; i++){
+        carouselPoint[i].style.cssText='width: 9px;height:9px;background:lightgray;border-radius:100%;margin:5px;z-index:2;cursor:pointer;'}
+    carouselPoint[_i].style.background='white'
 }
 
 setCarouselPointContainer()
